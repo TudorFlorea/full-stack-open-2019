f91fe56eb70bb8beaeeaa5bbf5049f794dad987d
TudorFlorea <ftudorel92@yahoo.com>
2019-10-09 21:56:13 +0300

    2.1: course contents step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ part }) => {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  const { name, parts } = course;
+  return (
+    <>
+      <Header name={name} />
+      {parts && parts.map(part => <Part key={part.id} part={part} />)}
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
