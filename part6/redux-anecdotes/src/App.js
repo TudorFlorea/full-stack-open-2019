2f1c5da8efa2d31ab56dfb454b32dda964b68fee
TudorFlorea <ftudorel92@yahoo.com>
2019-12-15 03:01:39 +0200

    6.3: anecdotes, step1

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const App = (props) => {
+  const {store} = props;
+  const anecdotes = store.getState()
+
+  const vote = (id) => {
+    console.log('vote', id)
+      store.dispatch({
+        type: "VOTE",
+        data: {
+          id
+        }
+      })
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
