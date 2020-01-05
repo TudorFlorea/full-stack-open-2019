213f63317db3701026d184bf62115b5b21d553f8
TudorFlorea <ftudorel92@yahoo.com>
2020-01-05 19:17:55 +0200

    8.4: Books of an author

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -105,7 +105,7 @@ type Query {
     hello: String!
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book]!
+    allBooks(author: String): [Book]
     allAuthors: [Author]!
   }
 `
@@ -115,7 +115,13 @@ const resolvers = {
     hello: () => { return "world" },
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+        if(args.author) {
+            return books.filter(book => book.author === args.author)
+        } else {
+            return books;
+        }
+    },
     allAuthors: () => {
         return authors.map(author => {
             const bookCount = books.filter(book => book.author === author.name).length;
