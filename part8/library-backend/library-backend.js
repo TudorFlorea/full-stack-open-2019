91083e09ba0f058ac5fe5bb270ed247647206031
TudorFlorea <ftudorel92@yahoo.com>
2020-01-05 18:55:23 +0200

    8.2: All books

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -85,10 +85,20 @@ let books = [
 ]
 
 const typeDefs = gql`
-  type Query {
+
+type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+}
+
+type Query {
     hello: String!,
     bookCount: Int!,
-    authorCount: Int!
+    authorCount: Int!,
+    allBooks: [Book]!
   }
 `
 
@@ -96,7 +106,8 @@ const resolvers = {
   Query: {
     hello: () => { return "world" },
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
